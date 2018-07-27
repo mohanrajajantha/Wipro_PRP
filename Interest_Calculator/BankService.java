2fa5dfd46c3aaf2a6bb032d628b542c8d90495a2
Elango R <30955305+elangoram1998@users.noreply.github.com>
2018-07-27 11:54:03 +0530

    Create BankService.java

---
+++ Interest_Calculator/BankService.java
@@ -0,0 +1,39 @@
+package com.wipro.bank.service;
+
+import com.wipro.bank.acc.RDAccount;
+import com.wipro.bank.exception.BankValidationException;
+
+public class BankService {
+	public void calculate(float principal,int tenure, int age, String gender)
+	{
+		if(validateData(principal,tenure,age,gender))
+		{
+		RDAccount r=new RDAccount(tenure, principal);
+		r.setInterest(age, gender);
+		System.out.println(r.calculateInterest());
+		System.out.println(r.calculateAmountDeposited());
+		System.out.println(r.calculateMaturityAmount(20*principal,r.calculateInterest()));
+		}
+	}
+	public boolean validateData(float principal, int tenure,int age, String gender) 
+	{
+		try
+		{
+		if(principal>=500&&(tenure==5||tenure==10)&&(gender.equals("male")||gender.equals("female"))&&(age>=1&&age<=100))
+		{
+			return true;
+		}
+		else
+		{
+			BankValidationException o=new BankValidationException();
+			System.out.println(o.toString());
+		}
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		return false;
+	}
+
+}
